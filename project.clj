146e29fa25d5f6f961da1719acce37758626fbd3
Thomas Meier <thomas.meier@citrine.it>
2015-06-17 14:47:35 +0000

    Set version and url and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-piton "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lein-piton "0.1.0"
+  :description "Leiningen plugin for managing piton SQL migrations"
+  :url "https://github.com/thomasmeier/lein-piton"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
